28e05499c468d1ebbdc7e5beb60134ea569a940e
limawebvision <devhenrike+limawebvision@gmail.com>
2024-11-14 03:16:04 -0300

    a

--- start.bat
+++ start.bat
@@ -3,5 +3,5 @@
 REM
 set FRONT_DIR=front
 set BACK_DIR=back
-start cmd /K "cd /d %FRONT_DIR% && npm run dev"
-start cmd /K "cd /d %BACK_DIR% && npm run start"
+start cmd /K "cd /d %FRONT_DIR% && npm run start"
+start cmd /K "cd /d %BACK_DIR% && npm run start:prod"
--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 FRONT_DIR="./front"
 BACK_DIR="./back"
 
-gnome-terminal --working-directory="$FRONT_DIR" -- bash -c "npm run dev; exec bash"
-gnome-terminal --working-directory="$BACK_DIR" -- bash -c "npm run start; exec bash"
+gnome-terminal --working-directory="$FRONT_DIR" -- bash -c "npm run start; exec bash"
+gnome-terminal --working-directory="$BACK_DIR" -- bash -c "npm run start:prod; exec bash"
